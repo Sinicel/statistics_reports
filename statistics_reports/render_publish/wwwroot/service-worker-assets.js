7c2304fe1d8a03d049e3c666743583143c8dacdb
asus auga <audrius.sinica@gmail.com>
2025-11-27 06:55:57 +0200

    fix_publish

--- statistics_reports/render_publish/wwwroot/service-worker-assets.js
+++ statistics_reports/render_publish/wwwroot/service-worker-assets.js
@@ -1,12 +1,12 @@
 self.assetsManifest = {
-  "version": "Z4gDEGxQ",
+  "version": "0Z+V4ab6",
   "assets": [
     {
       "hash": "sha256-1iKOJegOjo1RRPSy4m7hwDDn5fiZmx5qVl7e2i1a7Yo=",
       "url": "_framework/Microsoft.AspNetCore.Components.Forms.wasm"
     },
     {
-      "hash": "sha256-AQJJIOUqVCP4gzoeDCX2urVzmN0LfzbXaieQXGpEzTA=",
+      "hash": "sha256-NoVyixvtfUuMFj9LPNGphemwY8TjwobLjHg/IldYz7M=",
       "url": "_framework/Microsoft.AspNetCore.Components.Web.wasm"
     },
     {
@@ -166,7 +166,7 @@ self.assetsManifest = {
       "url": "_framework/System.wasm"
     },
     {
-      "hash": "sha256-EQGaDDe4HKD+KYN2sdoWDGzewooxSUbXeWeQWXFgKAw=",
+      "hash": "sha256-so7i8MJUYucEbKdlCmTbdYhnARROzevfa6wbG2jNjv4=",
       "url": "_framework/blazor.boot.json"
     },
     {
@@ -202,7 +202,7 @@ self.assetsManifest = {
       "url": "_framework/icudt_no_CJK.dat"
     },
     {
-      "hash": "sha256-uj/CV1M+GzdqepxQX69+5xfWvh2XQH71vg7lMgx/pS8=",
+      "hash": "sha256-GcWTlayQcYOQ1bO6ywAKpCvvmg+/FER8+QlXjQqSA8k=",
       "url": "_framework/statistics_reports.wasm"
     },
     {
--- statistics_reports/render_publish/wwwroot/service-worker.js
+++ statistics_reports/render_publish/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: Z4gDEGxQ */
+/* Manifest version: 0Z+V4ab6 */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
